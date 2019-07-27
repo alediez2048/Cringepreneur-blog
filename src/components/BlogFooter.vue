6c745d2431affec766034994152b13fcae662594
alediez2408 <alejandro@kordami.com>
2019-07-27 11:36:38 -0500

    no message

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Proudly powered by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
+        Proudly powered by <a href="https://latindigital.io" target="_blank">Latin Digital</a>
       </div>
     </footer>
   </transition>
